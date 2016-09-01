aea8f0b406d4d37183de35b6c3412e2c1d8ab5a1
Roman <roman@kamcord.com>
2016-09-01 13:40:17 -0700

    why would those be private

--- Classes/CAPSPageMenu.swift
+++ Classes/CAPSPageMenu.swift
@@ -105,8 +105,8 @@ public class CAPSPageMenu: UIViewController, UIScrollViewDelegate, UIGestureReco
     
     var selectionIndicatorView : UIView = UIView()
     
-    var currentPageIndex : Int = 0
-    var lastPageIndex : Int = 0
+    private(set) var currentPageIndex : Int = 0
+    private(set) var lastPageIndex : Int = 0
     
     public var selectionIndicatorColor : UIColor = UIColor.whiteColor()
     public var selectedMenuItemLabelColor : UIColor = UIColor.whiteColor()
